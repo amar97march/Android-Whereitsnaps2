20749c9f38e1ad599a9e8c456a2330287ba85117
amar97march <amar97march@gmail.com>
2017-10-22 15:29:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amar97march/whereitsnaps/TagsFragment.java
@@ -0,0 +1,15 @@
+package com.example.amar97march.whereitsnaps;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+
+public class TagsFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/amar97march/whereitsnaps/TitlesFragment.java
@@ -0,0 +1,16 @@
+package com.example.amar97march.whereitsnaps;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+public class TitlesFragment extends ListFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
